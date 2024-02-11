04870fd0e1f202599834a79712fac8fd4650b961
Imani Mwamlima <imwamlima@gmail.com>
2024-02-11 02:01:39 -0500

    add more of my custom commands from nightbot including having to add axios for the uptime command

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const tmi = require('tmi.js');
 require('dotenv').config();
 
@@ -25,48 +26,86 @@ client.on('connected', onConnectedHandler);
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
+function onMessageHandler (channel, user, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
 
+  // Get the username of the message sender
+  const senderName = user.username;
+
+  // if (commandName.length >= 1) {
+  //   console.log(user)
+  // }
+
   // If the command is known, let's execute it
   if (commandName === '!dice') {
     const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
+    client.say(channel, `You rolled a ${num}`);
     console.log(`* Executed ${commandName} command`);
   } 
   // HYPE Command
   if (commandName === '!hype') {
-    client.say(target, "renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype HYPE IN CHAT!!! renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype HYPE IN CHAT!!! renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype");
+    client.say(channel, "renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype HYPE IN CHAT!!! renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype HYPE IN CHAT!!! renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype renais9Hype");
     console.log(`* Executed ${commandName} command`);
   } 
 
   // BREAK Command
   if (commandName === '!break') {
-    client.say(target, "Everyone get up and stretch, hydrate, and any other relief you need! peepoPooPoo");
+    client.say(channel, "Everyone get up and stretch, hydrate, and any other relief you need! peepoPooPoo");
     console.log(`* Executed ${commandName} command`);
   }
 
   // RAID Command
   if (commandName === '!raid') {
-    client.say(target, ":D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID");
+    client.say(channel, ":D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID :D <3 :D RENAISANI RAID");
     console.log(`* Executed ${commandName} command`);
   }
 
   // CHAIR Command
   if (commandName === '!chair') {
-    client.say(target, "imani is currently contemplating life so he'll be right back pepeMeltdown");
+    client.say(channel, "imani is currently contemplating life so he'll be right back pepeMeltdown");
+    console.log(`* Executed ${commandName} command`);
+  }
+
+  // SOCIALS Command
+  if (commandName === '!socials') {
+    client.say(channel, "Follow the Renaisani story on social media! Instagram: https://www.instagram.com/renaisani/ Youtube: https://www.youtube.com/channel/UCCuGDNMxZ7m_C4fpxM5xqfQ/ Twitter: https://twitter.com/renaisani");
+    console.log(`* Executed ${commandName} command`);
+  }
+
+  // UPTIME Command
+  if (commandName === '!uptime') {
+
+    // Replace 'YOUR_API_ENDPOINT' with the actual Twitch API endpoint you want to call
+    const endpoint = 'https://beta.decapi.me/twitch/uptime/renaisani';
+
+    // Make a GET request
+    axios.get(endpoint)
+    .then(response => {
+      // Handle the response data here
+      client.say(channel, response.data);
+      console.log(`* Executed ${commandName} command`);
+    })
+    .catch(error => {
+      // Handle errors here
+      console.error('Axios error:', error);
+    });
+  }
+
+  // LURK Command
+  if (commandName === '!lurk') {
+    client.say(channel, senderName + " is now watching this adventure from afar while they are on their own adventure!");
     console.log(`* Executed ${commandName} command`);
   }
 
   if (commandName === 'what time is it?') {
-    client.say(target, "ADVENTURE TIME!!!");
+    client.say(channel, "ADVENTURE TIME!!!");
   }
 
   if (commandName === 'how old are you?') {
-    client.say(target, "I am 27!");
+    client.say(channel, "I am 27!");
   }
 }
 
